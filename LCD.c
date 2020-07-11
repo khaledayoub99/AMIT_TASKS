c50c9adb86497e874c942b6daaffdf6532930cd9
Khaled Mohamed Elsayed Ahmed <khaledayoup.2.2014@azhar.edu.eg>
2020-07-11 09:50:37 +0200

    new

--- LCD.c
+++ LCD.c
@@ -172,9 +172,6 @@ void LCD_GoTo 			(uint8 row, uint8 col)
 void LCD_WrtieNumber(int32 num )
 {
 	int32 temp = 0;
-	uint8 arr[5];
-	uint8 i=-1;
-	//uint8 digit;
 	temp =num;
 	if(temp<0)
 	{
@@ -183,12 +180,8 @@ void LCD_WrtieNumber(int32 num )
 	}
 	
 	while (temp > 0) {
-		i++;
-		arr[i] = temp % 10;
+		LCD_WrtieChar(temp % 10+48);
 		temp /= 10;
-	}	
-	for(i;i>=0;i--){
-		LCD_WrtieChar(arr[i] + 48);
 	}
 	
 }
